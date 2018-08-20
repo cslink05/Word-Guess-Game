6063d16d39f9e583aa29e8641e1cdf97f858da6b
Tim Lukens <tblukens87@gmail.com>
2018-08-19 21:40:58 -0500

    Game nearly 100% functional using objects. Could use small changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,47 +8,169 @@ var foodWords = ["lasagna",
 ];
 
 
-var wins = 0;
-var remainingGuesses = 12;
-var foodWord = "";
-var guessedLetters = [];
+var totalGuesses = 12;
 
 
-function randomFoodChoice() {
-    foodWord = foodWords[Math.floor(Math.random() * foodWords.length)];
-}
+// var wins = 0;
+// var foodWord = "";
+// var guessedLetters = [];
 
-randomFoodChoice();
 
-var answerArray = [];
-for (let i = 0; i < foodWord.length; i++) {
-    answerArray[i] = "_";
-}
+// function randomFoodChoice() {
+//     foodWord = foodWords[Math.floor(Math.random() * foodWords.length)];
+// }
 
-var italianFoodWords = document.getElementById("italianFoodWords");
+// randomFoodChoice();
 
-document.getElementById("italianFoodWords").innerHTML += answerArray.join(" ");
+// var answerArray = [];
+// for (let i = 0; i < foodWord.length; i++) {
+//     answerArray[i] = "_";
+// }
 
-document.onkeyup = function (event) {
+// var italianFoodWords = document.getElementById("italianFoodWords");
 
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
+// document.getElementById("italianFoodWords").innerHTML += answerArray.join(" ");
 
-        var userGuess = event.key;
+// document.onkeyup = function (event) {
 
-        for (var j = 0; j < foodWord.length; j++) {
-            if (foodWord[j] === userGuess) {
-                answerArray[j] = userGuess;
-                var element = document.getElementById("italianFoodWords");
-                element.innerHTML = answerArray.join(" ");
+//     if (event.keyCode >= 65 && event.keyCode <= 90) {
+
+//         var userGuess = event.key;
+
+//         for (var j = 0; j < foodWord.length; j++) {
+//             if (foodWord[j] === userGuess) {
+//                 answerArray[j] = userGuess;
+//                 var element = document.getElementById("italianFoodWords");
+//                 element.innerHTML = answerArray.join(" ");
+//             }
+//         }
+//     }
+
+
+// };
+
+const space = "space";
+
+var gameObject = {
+    foodWordsList: foodWords,
+    correctGuessedLetters: [],
+    incorrectGuessedLetters: [],
+    currentWord: "",
+    wins: 0,
+    remainingGuesses: totalGuesses,
+    gameOver: true,
+    lastWordIndex: -1,
+
+    // reset the game
+    resetGame: function () {
+
+        document.getElementById("gameOver").innerHTML = "";
+
+        var index = -1;
+
+        do {
+            index = Math.floor(Math.random() * this.foodWordsList.length);
+        } while( index === this.lastWordIndex);
+
+
+        this.currentWord = this.foodWordsList[index];
+        this.lastWordIndex = index;
+
+        this.correctGuessedLetters = [];
+        this.incorrectGuessedLetters = [];
+
+        this.remainingGuesses = totalGuesses;
+
+        this.gameOver = false;
+
+        var makeWord = this.currentWord.length;
+        for (var i = 0; i < makeWord; i++) {
+
+            this.correctGuessedLetters.push("_");
+        }
+
+        console.log(this.currentWord);
+        this.updatePage();
+    },
+
+
+    // update page html
+    updatePage: function () {
+        document.getElementById("wins").innerText = this.wins;
+
+        var holdWord = "";
+        for (let i = 0; i < this.correctGuessedLetters.length; i++) {
+            holdWord += this.correctGuessedLetters[i] + " ";
+        }
+
+        document.getElementById("italianFoodWords").innerHTML = holdWord;
+        document.getElementById("remainingGuesses").innerText = this.remainingGuesses;
+        document.getElementById("guessed").innerText = this.incorrectGuessedLetters;
+    },
+
+    // check if guess is correct
+
+    checkGuess: function (letter) {
+
+        var tempPositions = [];
+
+        for (let i = 0; i < this.currentWord.length; i++) {
+            if (this.currentWord[i] === letter) {
+                tempPositions.push(i);
             }
         }
+
+        if (tempPositions.length <= 0) {
+            this.remainingGuesses--;
+        } else {
+            for (let i = 0; i < tempPositions.length; i++) {
+                this.correctGuessedLetters[tempPositions[i]] = letter;
+            }
+        }
+    },
+
+
+    checkGameOver: function () {
+        if (this.correctGuessedLetters.indexOf("_") === -1) {
+            this.wins++
+            this.gameOver = true;
+            // alert("You won! The word was: " + this.currentWord.toUpperCase());
+            document.getElementById("gameOver").innerHTML = "<h1>YOU WIN!</h1><br><p>Press any key to play again!</p>";
+            return;
+        }
+        if (this.remainingGuesses <= 0) {
+            this.gameOver = true;
+            // alert("Sorry you lost. The word was: " + "'" + this.currentWord.toUpperCase() + "'" + " Press any key to try again.");
+            document.getElementById("gameOver").innerHTML = "<h1>Sorry you lose...</h1><br><p>Press any key to play again!</p>";
+            return;
+        }
+    },
+
+    makeGuess: function (letter) {
+        if (this.remainingGuesses > 0) {
+            if (this.incorrectGuessedLetters.indexOf(letter) === -1) {
+                this.incorrectGuessedLetters.push(letter);
+                this.checkGuess(letter);
+            }
+        }
+        this.checkGameOver();
+        this.updatePage();
     }
+
 
 
 };
 
-console.log(foodWord);
 
-// var game {
 
-// }
+// run game
+document.onkeyup = function (event) {
+    if (gameObject.gameOver) {
+        gameObject.resetGame();
+        gameObject.gameOver = false;
+    } else {
+        gameObject.makeGuess(event.key);
+    }
+}
+
+console.log(gameObject.currentWord);
